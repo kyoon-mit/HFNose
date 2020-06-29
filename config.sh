7e24955df07696be0c05e93fab90dc2e843c06d7
Kyungseop Yoon <yksns23@gmail.com>
2020-06-29 19:01:35 -0400

    more env var options

--- config.sh
+++ config.sh
@@ -1,4 +1,26 @@
 #!/usr/bin/env bash
 
+# WARNING: DO NOT MOVE THIS FILE. KEEP IT AT THE TOP-LEVEL DIRECTORY.
+
+
+###### WARNING: DO NOT CHANGE THESE ENVIRONMENT VARIABLES ######
+
+# This is the top directory
+export DIRTOP_HGCNOSE=$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )
+
+# Analysis directory
+export DIRANALYSIS_HGCNOSE=${DIRTOP_HGCNOSE}/Analysis
+
+# Simulation directory
+export DIRSIM_HGCNOSE=${DIRTOP_HGCNOSE}/FullSimulation
+
+################################################################
+
+
+
+###### YOU MAY CHANGE THESE ENVIRONMENT VARIABLES ##############
+
 # This is the directory where your simulation data files are stored.
-export DIR_DATA=<your path> # enter your prefered path here
+export DIRDATA_HGCNOSE=$PWD
+
+################################################################
