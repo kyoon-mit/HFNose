5107161aba0172e1629db49f75f0c1df1a4842ef
Kevin <kyoon@mit.edu>
2020-09-03 15:25:59 +0200

    updated phton generation files

--- FullSimulation/Single_Photon/photon_14TeV_2026D60_HGCNose/run.py
+++ FullSimulation/Single_Photon/photon_14TeV_2026D60_HGCNose/run.py
@@ -7,12 +7,12 @@ from MakeConfigFiles import *
 set_env()
 
 # Can also set up parser
-nevents = 50
-pt_string_list = set_pt('2')
+nevents = 10
+pt_string_list = set_pt('4','5','6','7')
 
 makeStep1ConfigFiles (pt_string_list, nevents)
 makeStep2ConfigFiles (pt_string_list, nevents)
 makeStep3ConfigFiles (pt_string_list, nevents)
 makeStep4ConfigFiles (pt_string_list, nevents)
 
-runSteps(pt_string_list, 3)
+runSteps(pt_string_list, 1,2,3)
