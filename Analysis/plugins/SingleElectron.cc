4d1a6910e0e59766f50c11e32c388c01daf9ef5f
Kevin <kyoon@mit.edu>
2020-09-16 23:00:59 +0200

    SingleElectron resolution and scale plots

--- Analysis/plugins/SingleElectron.cc
+++ Analysis/plugins/SingleElectron.cc
@@ -16,7 +16,8 @@
 // Physics objects
 // #include "DataFormats/HepMCCandidate/interface/GenParticle.h"
 #include "SimDataFormats/CaloAnalysis/interface/CaloParticle.h"
-#include "DataFormats/HGCalReco/interface/TICLCandidate.h"
+#include "DataFormats/HGCalReco/interface/Trackster.h"
+// #include "DataFormats/HGCalReco/interface/TICLCandidate.h"
 
 // ROOT headers
 
@@ -28,7 +29,8 @@ SingleElectron::SingleElectron ( const edm::ParameterSet& iConfig ) :
     // (tag name, default value (label, instance, process) -- CHECK SPELLING!!!!!!!
     // tag_GenParticle_ ( iConfig.getUntrackedParameter<edm::InputTag> ("TAG_GenParticle", edm::InputTag ("genParticles") ) ),
     tag_CaloParticle_MergedCaloTruth_ ( iConfig.getUntrackedParameter<edm::InputTag> ("TAG_MergedCaloTruth", edm::InputTag ("mix", "MergedCaloTruth") ) ),
-    tag_TICLCandidate_ ( iConfig.getUntrackedParameter<edm::InputTag> ("TAG_TICLCandidate", edm::InputTag ("ticlCandidateFromTracksters") ) ),
+    tag_Trackster_HFNoseTrk_ ( iConfig.getUntrackedParameter<edm::InputTag> ("TAG_Trackster", edm::InputTag ("ticlTrackstersHFNoseTrk") ) ),
+    // tag_TICLCandidate_ ( iConfig.getUntrackedParameter<edm::InputTag> ("TAG_TICLCandidate", edm::InputTag ("ticlCandidateFromTracksters") ) ),
     
     // Pre-selection parameters
     select_PID_ ( 11 ),
@@ -38,7 +40,8 @@ SingleElectron::SingleElectron ( const edm::ParameterSet& iConfig ) :
     // consumes: frequent request of additional data | mayConsume: infrequent
     // token_GenParticle_ = consumes<reco::GenParticleCollection> ( tag_GenParticle_ );
     token_CaloParticle_MergedCaloTruth_ = mayConsume<std::vector<CaloParticle>> ( tag_CaloParticle_MergedCaloTruth_ );
-    token_TICLCandidate_ = consumes<std::vector<TICLCandidate>> ( tag_TICLCandidate_ );
+    token_Trackster_HFNoseTrk_ = consumes<std::vector<ticl::Trackster>> ( tag_Trackster_HFNoseTrk_ );
+    // token_TICLCandidate_ = consumes<std::vector<TICLCandidate>> ( tag_TICLCandidate_ );
 }
 
 
@@ -57,40 +60,39 @@ void SingleElectron::analyze ( const edm::Event& iEvent, const edm::EventSetup& 
     iEvent.getByToken ( token_CaloParticle_MergedCaloTruth_, handle_CaloParticle_MergedCaloTruth );
     
     // Get TICLCandidate
-    edm::Handle<std::vector<TICLCandidate>> handle_TICLCandidate;
-    iEvent.getByToken ( token_TICLCandidate_, handle_TICLCandidate );
+    // edm::Handle<std::vector<TICLCandidate>> handle_TICLCandidate;
+    // iEvent.getByToken ( token_TICLCandidate_, handle_TICLCandidate );
     
-    if ( handle_CaloParticle_MergedCaloTruth.isValid() && handle_TICLCandidate.isValid() )
+    // Get ticlTrackstersHFNoseTrk
+    edm::Handle<std::vector<ticl::Trackster>> handle_Trackster_HFNose_Trk;
+    iEvent.getByToken ( token_Trackster_HFNoseTrk_, handle_Trackster_HFNose_Trk );
+    
+    if ( handle_CaloParticle_MergedCaloTruth.isValid() && handle_Trackster_HFNose_Trk.isValid() )
     {
 
-        const std::vector<math::XYZTLorentzVectorF> truth_container = getTruthP4 ( *handle_CaloParticle_MergedCaloTruth.product() );
-        const std::vector<math::XYZTLorentzVectorF> reco_container = getTICLCandidateP4 ( *handle_TICLCandidate.product() );
+        // const std::vector<math::XYZTLorentzVectorF> truth_container = getTruthP4 ( *handle_CaloParticle_MergedCaloTruth.product() );
+        analyzeTICLTrackster ( *handle_CaloParticle_MergedCaloTruth.product(), *handle_Trackster_HFNose_Trk.product() );
         
-        for ( auto const& truth: truth_container )
+        /*for ( auto const& truth: truth_container )
         {            
             // fillHist_HGCalRecHitsEnergy_coneR ( truth, *handle_HGCRecHits.product(), handle_HGCalGeometry.product() );
             // fillHist_CaloClustersEnergy_coneR ( truth, *handle_HGCalLayerClustersHFNose.product() );
             // histContainer_["truthEDist"]->Fill ( truth.energy() );
-            std::cout << "CaloParticle energy:" << truth.energy() << std::endl;
-        }
-        
-        for ( auto const& rc: reco_container )
-        {
-            std::cout << "TICL Candidate mass:" << rc.M() << std::endl;
-        }
+            
+        }*/
         
     }
     else std::cout << "Handle(s) invalid!" << std::endl;
 }
 
 
-std::vector<math::XYZTLorentzVectorF> SingleElectron::getTruthP4 ( const std::vector<CaloParticle> & caloTruthParticle )
+std::vector<math::XYZTLorentzVectorF> SingleElectron::getTruthP4 ( const std::vector<CaloParticle> & caloTruthParticles )
 {
     std::vector<math::XYZTLorentzVectorF> container;
     
-    for ( auto const& ct: caloTruthParticle )
+    for ( auto const& ct: caloTruthParticles )
     {
-        if ( ct.pdgId() == select_PID_
+        if ( abs(ct.pdgId()) == select_PID_
                 && abs(ct.eta()) > select_EtaLow_
                 && abs(ct.eta()) < select_EtaHigh_ )
         {
@@ -102,6 +104,41 @@ std::vector<math::XYZTLorentzVectorF> SingleElectron::getTruthP4 ( const std::ve
 }
 
 
+void SingleElectron::analyzeTICLTrackster ( const std::vector<CaloParticle> & caloTruthParticles, const std::vector<ticl::Trackster> & tracksters )
+{
+
+    const std::vector<math::XYZTLorentzVectorF> selected_calotruths = getTruthP4 ( caloTruthParticles );
+
+    for ( auto const& trs: tracksters )
+    {
+        Float_t trackster_raw_energy = trs.raw_energy();
+        Float_t trackster_eta = trs.barycenter().eta();
+        
+        for ( auto const& clt: selected_calotruths )
+        {
+            histContainer_["truthE"]->Fill( clt.E() );
+            histContainer_["truthEta"]->Fill( clt.Eta() );
+            
+            if ( clt.Eta() > 0 && trackster_eta > 0 )
+            {
+                histContainer_["tracksterRawEDist"]->Fill( trackster_raw_energy );
+                histContainer_["tracksterAbsEtaDist"]->Fill( abs(trackster_eta) );
+                histContainer_["tracksterRawEScale"]->Fill( clt.E() - trackster_raw_energy );
+                histContainer_["tracksterAbsEtaScale"]->Fill( abs(clt.Eta() - trackster_eta) );
+            }
+            else if ( clt.Eta() < 0 && trackster_eta < 0 )
+            {
+                histContainer_["tracksterRawEDist"]->Fill( trackster_raw_energy );
+                histContainer_["tracksterAbsEtaDist"]->Fill( abs(trackster_eta) );
+                histContainer_["tracksterRawEScale"]->Fill( clt.E() - trackster_raw_energy );
+                histContainer_["tracksterAbsEtaScale"]->Fill( abs(clt.Eta() - trackster_eta) );
+            }
+        }
+    }
+}
+
+
+/*
 std::vector<math::XYZTLorentzVectorF> SingleElectron::getTICLCandidateP4 ( const std::vector<TICLCandidate> & TICLCandidates )
 {
     std::vector<math::XYZTLorentzVectorF> container;
@@ -118,13 +155,25 @@ std::vector<math::XYZTLorentzVectorF> SingleElectron::getTICLCandidateP4 ( const
     
     return container;
 }
+*/
 
 
 void SingleElectron::beginJob ()
 {
     edm::Service<TFileService> fs;
     
-    // histContainer_["truthEDist"] = fs->make<TH1F>("truthEDist", "Truth Energy Distribution", 200, 0, 200);
+    histContainer_["truthE"] = fs->make<TH1F>("truthE", "Truth Energy Distribution", 100, 0, 1000);
+    histContainer_["truthEta"] = fs->make<TH1F>("truthEta", "Truth Eta Distribution", 40, 3.0, 4.0);
+    histContainer_["tracksterRawEDist"] = fs->make<TH1F>("tracksterRawEDist", "Trackster Raw Energy Distribution", 100, 0, 1000);
+    histContainer_["tracksterAbsEtaDist"] = fs->make<TH1F>("tracksterAbsEtaDist", "Trackster Absolute Eta Distribution", 40, 3.0, 4.0);
+    histContainer_["tracksterRawEScale"] = fs->make<TH1F>("tracksterRawEScale", "Trackster Raw Energy Scale", 20, 0, 100);
+    histContainer_["tracksterAbsEtaScale"] = fs->make<TH1F>("tracksterAbsEtaScale", "Trackster Absolute Eta Scale", 20, 0, 0.5);
+    
+    // Label axes
+    histContainer_["tracksterRawEDist"]->GetXaxis()->SetTitle("E_{trackster} [GeV/c^{2}]");
+    histContainer_["tracksterAbsEtaDist"]->GetXaxis()->SetTitle("|#eta_{trackster}|");
+    histContainer_["tracksterRawEScale"]->GetXaxis()->SetTitle("E_{trackster}-E_{caloParticle} [GeV/c^{2}]");
+    histContainer_["tracksterAbsEtaScale"]->GetXaxis()->SetTitle("|#eta_{trackster} - #eta_{caloParticle}| [GeV/c^{2}]");
 }
 
 
--- Analysis/plugins/SingleElectron.h
+++ Analysis/plugins/SingleElectron.h
@@ -23,15 +23,20 @@
 #include "DataFormats/Math/interface/LorentzVector.h"
 
 // Forward declarations
-// class TH1F;
+class TH1F;
 
 class CaloParticle;
-class TICLCandidate;
+// class TICLCandidate;
 
 namespace reco
 {
     // class GenParticle;
     // typedef std::vector<GenParticle> GenParticleCollection;
+}
+
+namespace ticl
+{
+    class Trackster;
 }
 
 
@@ -49,22 +54,24 @@ class SingleElectron : public edm::EDAnalyzer
     virtual void endJob () override;
   
     std::vector<math::XYZTLorentzVectorF> getTruthP4 ( const std::vector<CaloParticle> & );
-    std::vector<math::XYZTLorentzVectorF> getTICLTracksterP4 ( );
-    std::vector<math::XYZTLorentzVectorF> getTICLCandidateP4 ( const std::vector<TICLCandidate> & );
+    void analyzeTICLTrackster ( const std::vector<CaloParticle> &, const std::vector<ticl::Trackster> & );
+    // std::vector<math::XYZTLorentzVectorF> getTICLCandidateP4 ( const std::vector<TICLCandidate> & );
 
     // Container
-    // std::map <std::string, TH1F*> histContainer_; // map of histograms
+    std::map <std::string, TH1F*> histContainer_; // map of histograms
 
     // ------ Data members -------
     // Tokens
     // edm::EDGetTokenT<reco::GenParticleCollection> token_GenParticle_;
     edm::EDGetTokenT<std::vector<CaloParticle>> token_CaloParticle_MergedCaloTruth_;
-    edm::EDGetTokenT<std::vector<TICLCandidate>> token_TICLCandidate_;
+    edm::EDGetTokenT<std::vector<ticl::Trackster>> token_Trackster_HFNoseTrk_;
+    // edm::EDGetTokenT<std::vector<TICLCandidate>> token_TICLCandidate_;
     
     // Input Tags
     // edm::InputTag tag_GenParticle_;
     edm::InputTag tag_CaloParticle_MergedCaloTruth_;
-    edm::InputTag tag_TICLCandidate_;
+    edm::InputTag tag_Trackster_HFNoseTrk_;
+    // edm::InputTag tag_TICLCandidate_;
     
     // Others
     Int_t   select_PID_;
